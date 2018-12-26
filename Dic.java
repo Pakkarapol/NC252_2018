624430d51bcd645ebf552d9dfce6d285ec7422cd
Pakkarapol <42764013+Pakkarapol@users.noreply.github.com>
2018-12-26 21:19:43 +0700

    Add files via upload

---
+++ Dic.java
@@ -0,0 +1,29 @@
+
+	public class Dic{
+		private String Conver;
+		private String Mean;
+		
+
+
+	public Dic(String Conver,String Mean) {
+		this.Conver = Conver;
+		this.Mean = Mean;
+	}
+
+	public String getConver() {
+		return Conver;
+	}
+
+	public void setConver(String conver) {
+		Conver = conver;
+	}
+	
+	public String getMean() {
+		return Mean;
+	}
+
+	public void setMean(String mean) {
+		Mean = mean;
+	}
+	}
+
---
+++ LinkList.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LinkList {
+	private LinkList First;
+	public int Num;
+	public LinkList next;
+	public LinkList previous;
+
+	
+public LinkList(int Num) {
+	this.Num = Num;
+}
+
+	
+
+ 
+}
+ 
